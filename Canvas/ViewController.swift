ccd7e45fca1586b9476b77a7c0143c83b64b04eb
alimir1 <contact@alimir.io>
2017-10-04 20:00:43 -0700

    3rd milestone

--- Canvas/ViewController.swift
+++ Canvas/ViewController.swift
@@ -14,26 +14,39 @@ class ViewController: UIViewController {
     
     var originalCenter: CGPoint!
     
+    var trayCenterWhenOpen: CGPoint!
+    var trayCenterWhenClosed: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        trayCenterWhenOpen = trayView.center
+        trayCenterWhenClosed = CGPoint(x: trayView.center.x, y: trayView.center.y + 200)
     }
     
     @IBAction func onTrayPanGesture(_ panGestureRecognizer: UIPanGestureRecognizer) {
-        let point = panGestureRecognizer.location(in: self.view)
+//        let point = panGestureRecognizer.location(in: self.view)
         
         let translation = panGestureRecognizer.translation(in: self.view)
+        let velocity = panGestureRecognizer.velocity(in: view)
         
         switch panGestureRecognizer.state {
         case .began:
             originalCenter = trayView.center
         case .changed:
             trayView.center = CGPoint(x: originalCenter.x, y: translation.y + originalCenter.y)
-            break;
         case .ended:
-            break;
-        default:
-            break;
+            
+            UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.5, options: [], animations: {
+                if velocity.y < 0 {
+                    self.trayView.center = self.trayCenterWhenOpen
+                } else {
+                    self.trayView.center = self.trayCenterWhenClosed
+                }
+                self.view.layoutIfNeeded()
+            }, completion: nil)
+            
+        default: break
         }
         
     }
